a6cb5f587f712a3baa00ea167aa6503caf8fb3f0
leonwind <leon.windheuser@gmail.com>
2018-09-18 17:11:14 +0200

    Remove all scratch, message or completions buffer and save session after closing emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -86,3 +86,30 @@
 ;; get multiple cursor
 (global-set-key (kbd "C-{") 'mc/mark-previous-like-this)
 (global-set-key (kbd "C-}") 'mc/mark-next-like-this)
+
+;; save emacs session
+(desktop-save-mode 1)
+
+
+;; Removes *scratch* from buffer after the mode has been set.
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+;; Removes *messages* from the buffer.
+(setq-default message-log-max nil)
+(kill-buffer "*Messages*")
+
+;; Removes *Completions* from buffer after you've opened a file.
+(add-hook 'minibuffer-exit-hook
+      '(lambda ()
+         (let ((buffer "*Completions*"))
+           (and (get-buffer buffer)
+                (kill-buffer buffer)))))
+
+;; Don't show *Buffer list* when opening multiple files at the same time.
+(setq inhibit-startup-buffer-menu t)
+
+;; Show only one active window when opening multiple files at the same time.
+(add-hook 'window-setup-hook 'delete-other-windows)
