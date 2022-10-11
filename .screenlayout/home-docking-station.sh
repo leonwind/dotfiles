4fc69a7b25cf5908562eac49a1dfa8854ec6d15c
Leon Windheuser <leon.windheuser@gmail.com>
2022-10-11 16:37:03 +0200

    Add screenlayout options

---
+++ .screenlayout/home-docking-station.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-5 --mode 1920x1080 --pos 0x0 --rotate right --output DP-2-6 --primary --mode 1920x1080 --pos 1080x494 --rotate normal
