00f7c84059f60bcd86095675a9f7a72d219098d8
Leon Windheuser <leon.windheuser@gmail.com>
2024-07-30 17:39:07 +0200

    Enable bluetooth on boot

--- .config/nix-config/hardware-configuration.nix
+++ .config/nix-config/hardware-configuration.nix
@@ -34,4 +34,5 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
 }
