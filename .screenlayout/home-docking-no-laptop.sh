8f168eac590246dfc3af1133bf22ceca40808764
Leon Windheuser <leon.windheuser@gmail.com>
2024-11-07 22:19:17 +0100

    Reset rate to 60

--- .screenlayout/home-docking-no-laptop.sh
+++ .screenlayout/home-docking-no-laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-5 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --rate 50 --output DP-2-6 --mode 1920x1080 --pos 0x0 --rotate right --rate 50
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-5 --primary --mode 1920x1080 --pos 1080x420 --rotate normal --output DP-2-6 --mode 1920x1080 --pos 0x0 --rotate right
