879214fab77eb4ddfb0a3e10feb5da555499860d
Leon Windheuser <leon.windheuser@gmail.com>
2024-12-10 19:34:23 +0100

    Add home manager files in new path

---
+++ .config/home-manager/home.nix
@@ -0,0 +1,91 @@
+{ config, pkgs, ... }:
+
+{
+	imports = [
+		./vim.nix
+		./zsh.nix
+	];
+
+	home.username = "leon";
+	home.homeDirectory = "/home/leon";
+	nixpkgs.config.allowUnfree = true;
+
+    home.stateVersion = "24.05";
+	programs.home-manager.enable = true;
+
+    home.packages = with pkgs; [
+        openvpn
+        gnome.gnome-keyring
+        libsecret 
+        libgnome-keyring
+        xclip
+        ncdu
+        shutter
+        nethogs
+        blueman
+        tmux
+        htop
+        docker
+        firefox
+        google-chrome
+        rxvt_unicode
+        tmux
+        source-code-pro
+		oh-my-zsh	
+        rofi
+        pass
+        rofi-pass
+		neofetch
+		arandr
+        redshift
+        fzf
+        tlp
+        cloc
+        tree
+        pamixer
+        evince
+        zip
+        unzip
+        go-chromecast
+        #steam
+        scrot
+        networkmanagerapplet
+        vscode
+        poppler_utils
+        pavucontrol
+
+        jetbrains.clion
+        
+        python3
+        go
+
+        anki-bin
+        xfce.thunar
+        
+        linuxKernel.packages.linux_zen.perf
+
+        prismlauncher
+    ];
+    
+    services.redshift = {
+        enable = true;
+        # Lat. and long. of Munich 
+        latitude = "48.14";
+        longitude = "11.57";
+        temperature = {
+            day = 6500;
+            night = 3000;
+        };
+    };
+
+    #services.openvpn.servers = {
+    #    eduVPN = { 
+    #        config = "~/openvpn//tum.eduvpn.lrz.de_tum-full-ov_20241111_eduvpn-cert.ovpn"; 
+    #    };
+    #};
+    
+    programs.go = {
+        enable = true;
+        goPath = "go";
+    };
+}
---
+++ .config/home-manager/zsh.nix
@@ -0,0 +1,56 @@
+{
+	programs.zsh = {
+		enable = true;
+		oh-my-zsh = {
+			enable = true;
+			theme = "agnoster";
+        };
+        
+        autosuggestion.enable = true;
+        
+        sessionVariables = rec {
+            EDITOR = "vim";
+            GIT_EDITOR = EDITOR;
+            CLICOLOR = "YES";
+        };
+
+        initExtra = ''
+            export PATH="$PATH:$HOME/go/bin"
+            prompt_context(){}
+            source "$(fzf-share)/key-bindings.zsh"
+            source "$(fzf-share)/completion.zsh"
+        '';
+        
+        zplug = {
+            enable = true;
+            plugins = [
+                {name = "agkozak/zsh-z";}
+                {name = "junegunn/fzf";}
+                {name = "zsh-users/zsh-syntax-highlighting";}
+            ];
+        };
+
+        shellAliases = {
+            hm = "home-manager";
+        
+            gits = "git status";
+            gita = "git add -A";
+            gitc = "git commit -m";
+            gitd = "git diff";
+            gitds = "git diff --stat";
+            gitl = "git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+            nsg = "nix-shell-generator";
+            
+            chrome = "google-chrome-stable";
+            lxprint =  "ssh ge57dov@lxhalle.in.tum.de lpr <";
+
+            idea = "idea-ultimate";
+
+            perf-stats = "perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations";
+
+            copy = "xclip -selection clipboard";
+            fvim = "vim $(fzf)";
+            btw = "neofetch";
+        };
+	};
+}
