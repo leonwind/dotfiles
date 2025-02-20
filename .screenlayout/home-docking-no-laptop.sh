829e9b0de555565024a887289824162c05a00dcc
Leon Windheuser <leon.windheuser@gmail.com>
2025-02-20 20:45:05 +0100

    Switch main and second monitor position

--- .screenlayout/home-docking-no-laptop.sh
+++ .screenlayout/home-docking-no-laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-8 --mode 2560x1440 --pos 1080x174 --rotate normal --output DP-2-1 --off --output DP-2-1-1 --mode 1920x1080 --pos 0x0 --rotate right
+xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --off --output DP-2-8 --mode 2560x1440 --pos 0x207 --rotate normal --output DP-2-1 --off --output DP-2-1-1 --mode 1920x1080 --pos 2560x0 --rotate left
